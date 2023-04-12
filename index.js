5f74742cd093c0e36a904bfd5a24f1fdc4040d1e
Daniel Teles <dan.ribeiro@hotmail.com>
2023-04-12 01:05:01 -0300

    Course provided by Meta - Exercise: Repetitive tasks with loops

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ if (age >= 65) {
 }
 */
 
+/*
 var day = "Friday";
 
 switch (day) {
@@ -39,4 +40,50 @@ switch (day) {
     break;
   default:
     console.log("The is no such day");
+}
+*/
+
+/*
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("Counting Completed!");
+*/
+
+/*
+for (let i = 5; i >= 1; i--) {
+  console.log(i);
+}
+
+console.log("Countdown finished!");
+*/
+
+/*
+var i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+console.log("Counting completed!");
+*/
+
+/*
+var i = 5;
+
+while (i >= 1) {
+  console.log(i);
+  i = i - 1;
+}
+
+console.log("Countdown finished!");
+*/
+
+var year = 2018;
+
+while (year <= 2022) {
+  console.log(year);
+  year++;
 }

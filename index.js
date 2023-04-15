e570f33d8d3bf973150c0fcc8d1998558eb7f361
Daniel Teles <dan.ribeiro@hotmail.com>
2023-04-14 23:29:29 -0300

    Practicing - Exercise - Try...catch blocks

--- index.js
+++ index.js
@@ -268,7 +268,27 @@ console.log("My program does not stop");
 
 // Undefined, null and empty values
 
+/*
 console.log(noise2); // Output: Uncaught ReferenceError: noise2 is not defined
 
 var word;
 console.log(word); // Output: undefined
+*/
+
+// Exercise - Error prevention
+
+function addTwoNums(a, b) {
+  try {
+    if (typeof a !== "number") {
+      throw new ReferenceError("the first argument is not a number");
+    } else if (typeof b !== "number") {
+      throw new ReferenceError("the second argument is not a number");
+    } else {
+      console.log(a + b);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
+}
+addTwoNums(5, "5");
+console.log("It still works");
